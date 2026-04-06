9cda678cc437f88de28a3ea0314a7eea934101ab
a-molito <molito@patec.online>
2026-04-06 14:08:54 +0200

    fix: correct load_dotenv import typo (was load_doten)

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ from urllib.parse import quote
 from fastapi import FastAPI, Request, Header, HTTPException
 from starlette.middleware.base import BaseHTTPMiddleware
 import httpx
-from dotenv import load_doten
+from dotenv import load_dotenv
 from slowapi import Limiter, _rate_limit_exceeded_handler
 from slowapi.util import get_remote_address
 from slowapi.errors import RateLimitExceeded
